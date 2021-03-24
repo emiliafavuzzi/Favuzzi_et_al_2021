a358f63a547d12d4cb46ad753f7be8661910fad5
lbinan <79212104+lbinan@users.noreply.github.com>
2021-03-24 17:42:41 -0400

    Create ExtractCells2plot.m

---
+++ ExtractCells2plot.m
@@ -0,0 +1,26 @@
+function [myList]=ExtractCells2plot(myclustergreen,myslice)
+load('listsOfcells.mat')
+MyTable(:,1)=table2array(ctrkoinfoseparateCTRKO(:,1));
+MyTable(:,2)=table2array(ctrkoinfoseparateCTRKO(:,3));
+MyTable(:,3)=table2array(ctrkoinfoseparateCTRKO(:,4));
+MyTable(:,4)=table2array(ctrkoinfoseparateCTRKO(:,22));
+MyTable(:,5)=table2array(ctrkoinfoseparateCTRKO(:,34));
+MyTable(:,6)=table2array(ctrkoinfoseparateCTRKO(:,35));
+MyTable(:,7)=table2array(ctrkoinfoseparateCTRKO(:,36));
+
+
+myList=[];
+for i=1:size(MyTable,1)
+    if MyTable(i,5)==myslice%slice1side2
+        if MyTable(i,6)==myclustergreen
+            myList=[myList;MyTable(i,1:7)];
+        end
+    end
+end
+myMinimum=0;
+for ii=1:size(MyTable,1)
+    if MyTable(ii,5)==myslice && myMinimum==0
+        myMinimum=ii;
+    end
+end
+myList(:,1)=myList(:,1)-myMinimum+1;
