45598008d3cd9fb0cab4f6fedb07cb894a88d0c1
lbinan <79212104+lbinan@users.noreply.github.com>
2021-03-24 17:38:06 -0400

    Update Main_call_analysis.m

--- Main_call_analysis.m
+++ Main_call_analysis.m
@@ -3,78 +3,78 @@ disp('running KO2_brain2');
 disp('slice1_side1');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO2_brain2/slice1_side1');
 functiongenerateCountMatrixOnClusterMoreTmem('KO2_brain2/slice1_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO2_brain2/slice1_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO2_brain2/slice1_side1',cellmaskTmem119);
 clear
 disp('slice1_side2');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO2_brain2/slice1_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('KO2_brain2/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
- functiongenerateIntensityMatrixOnClusterMoreTmem('KO2_brain2/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+ fastintensity('KO2_brain2/slice1_side2',cellmaskTmem119);
 
 clear
 disp('slice2_side1');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO2_brain2/slice2_side1');
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO2_brain2/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateCountMatrixOnClusterMoreTmem('KO2_brain2/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO2_brain2/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+functiongenerateCountMatrixOnClusterMoreTmem('KO2_brain2/slice2_side1',cellmaskTmem119);
 
 clear
 disp('slice2_side2');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO2_brain2/slice2_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('KO2_brain2/slice2_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO2_brain2/slice2_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO2_brain2/slice2_side2',cellmaskTmem119);
 
 clear
 disp('slice3_side1');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO2_brain2/slice3_side1');
 functiongenerateCountMatrixOnClusterMoreTmem('KO2_brain2/slice3_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO2_brain2/slice3_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO2_brain2/slice3_side1',cellmaskTmem119);
 clear
 disp('slice3_side2');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control1_brain2/slice3_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('control1_brain2/slice3_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('control1_brain2/slice3_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('control1_brain2/slice3_side2',cellmaskTmem119);
 clear
 clear
 disp('running KO1_brain3');
 disp('slice1_side1');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice1_side1');
 functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice1_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice1_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO1_brain3/slice1_side1',cellmaskTmem119);
 clear
 disp('slice1_side2');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice1_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
- functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+ fastintensity('KO1_brain3/slice1_side2',cellmaskTmem119);
 % 
 clear
 disp('slice2_side1');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice2_side1');
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO1_brain3/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice2_side1',cellmaskTmem119);
 
 clear
 disp('slice1_side2');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice2_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice2_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice2_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO1_brain3/slice2_side2',cellmaskTmem119);
 
 clear
 disp('slice3_side1');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice3_side1');
 functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice3_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice3_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO1_brain3/slice3_side1',cellmaskTmem119);
 clear
 disp('slice3_side2');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice3_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice3_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice3_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('KO1_brain3/slice3_side2',cellmaskTmem119);
 clear
 %
 
@@ -83,39 +83,39 @@ disp('running control2_brain3');
 disp('slice1_side1');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control2_brain3/slice1_side1');
 functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice1_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('control2_brain3/slice1_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('control2_brain3/slice1_side1',cellmaskTmem119);
 clear
 disp('slice1_side2');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control2_brain3/slice1_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
- functiongenerateIntensityMatrixOnClusterMoreTmem('control2_brain3/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+ fastintensity('control2_brain3/slice1_side2',cellmaskTmem119);
 
 clear
 disp('slice2_side1');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control2_brain3/slice2_side1');
-functiongenerateIntensityMatrixOnClusterMoreTmem('control2_brain3/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('control2_brain3/slice2_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice2_side1',cellmaskTmem119);
 
 clear
 disp('slice2_side2');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control2_brain3/slice2_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice2_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('control2_brain3/slice2_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('control2_brain3/slice2_side2',cellmaskTmem119);
 
 clear
 disp('slice3_side1');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control2_brain3/slice3_side1');
 functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice3_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('control2_brain3/slice3_side1',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('control2_brain3/slice3_side1',cellmaskTmem119);
 clear
 disp('slice3_side2');
 
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('control2_brain3/slice3_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('control2_brain3/slice3_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
-functiongenerateIntensityMatrixOnClusterMoreTmem('control2_brain3/slice3_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+fastintensity('control2_brain3/slice3_side2',cellmaskTmem119);
 clear
 %clear
 
@@ -123,6 +123,6 @@ clear
 disp('trying KO1_brain3 slice1_side2');
 [cellmaskTmem119,cellmaskallTmem_Gabbr1_2]=functionmaskeMasksOnClusterMoreTmem('KO1_brain3/slice1_side2');
 functiongenerateCountMatrixOnClusterMoreTmem('KO1_brain3/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
- functiongenerateIntensityMatrixOnClusterMoreTmem('KO1_brain3/slice1_side2',cellmaskTmem119,cellmaskallTmem_Gabbr1_2);
+ fastintensity('KO1_brain3/slice1_side2',cellmaskTmem119);
 % 
 clear
