000203a61633b3013e6cb54afcf9e26e02ca9859
lbinan <79212104+lbinan@users.noreply.github.com>
2021-03-25 09:10:49 -0400

    Create maskFOV.m

---
+++ maskFOV.m
@@ -0,0 +1,4 @@
+%script used to generate a mask of the region of interest, avoiding counts from cell outside of it. save the variable space as matlab.mat after running it
+figure; imshow(5*mosaic_DAPI_3, []);
+roi = images.roi.AssistedFreehand;
+draw(roi);mask = createMask(roi);imshow(mask)
