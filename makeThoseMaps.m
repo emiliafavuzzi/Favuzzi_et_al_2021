7124d245d2a89a825bdd4010b393117028226b33
lbinan <79212104+lbinan@users.noreply.github.com>
2021-03-26 15:40:49 -0400

    Update makeThoseMaps.m

--- makeThoseMaps.m
+++ makeThoseMaps.m
@@ -1,5 +1,7 @@
  function [mythreshold]=makeThoseMaps(thatpath,myList,myclustergreen)
-
+% Loic Binan
+%lbinan@broadinstitute.org
+%3/26/2021
 % thatpath='control1_brain2/slice1_side1';
 savepath=fullfile('/broad/hptmp/lbinan/microglia/',thatpath);
 mypath=fullfile(savepath,'/merfish_mosaics');
